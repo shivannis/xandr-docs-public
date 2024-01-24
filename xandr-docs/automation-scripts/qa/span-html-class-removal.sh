b7bd078779801db27acb77f5a21a4b385006a3ab
Shivanni S <144895768+shivannis@users.noreply.github.com>
2024-01-24 15:58:50 +0530

    Update span-html-class-removal.sh

--- xandr-docs/automation-scripts/qa/span-html-class-removal.sh
+++ xandr-docs/automation-scripts/qa/span-html-class-removal.sh
@@ -14,13 +14,13 @@ remove_span_tags() {
     for input_file in "$@"; do
         # Create a temporary file
         temp_file="${input_file}.temp"
-        
+
         # Use sed to remove <span>, <span class="ph">, and </span> tags from the file and save it to the temporary file
         sed 's/<span>//g; s/<\/span>//g; s/<span class="ph">//g; s/<\/span>//g' "$input_file" > "$temp_file"
-        
+
         # Replace the original file with the modified file
         mv "$temp_file" "$input_file"
-        
+
         echo "Removed <span>, <span class=\"ph\">, and </span> tags from: $input_file"
     done
 }
@@ -29,4 +29,4 @@ export -f remove_span_tags
 
 # Recursively find and remove tags in files
 find "$input_dir" -type f -exec bash -c 'remove_span_tags "$0"' {} \;
-
+echo
